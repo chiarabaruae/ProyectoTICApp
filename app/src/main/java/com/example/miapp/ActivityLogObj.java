6d8e16023201ffa520076ab73a45d34197403d3f
chiarabaruae <chiara.barua@upa.edu.py>
2021-11-04 22:30:14 -0300

    3° commit

--- app/src/main/java/com/example/miapp/ActivityLogObj.java
+++ app/src/main/java/com/example/miapp/ActivityLogObj.java
@@ -1,8 +1,15 @@
 package com.example.miapp;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ActivityLogObj extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_item);
 
+    }
 
 }
