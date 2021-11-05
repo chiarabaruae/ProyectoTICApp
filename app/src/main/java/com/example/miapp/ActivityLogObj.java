2a216544a3d3ef16b912305e6d56b8775e0c3df2
chiarabaruae <77409443+chiarabaruae@users.noreply.github.com>
2021-11-05 10:34:18 -0300

    5° commit

--- app/src/main/java/com/example/miapp/ActivityLogObj.java
+++ app/src/main/java/com/example/miapp/ActivityLogObj.java
@@ -1,6 +1,8 @@
 package com.example.miapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,8 +10,11 @@ public class ActivityLogObj extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_item);
+        setContentView(R.layout.activity_lis_item2);
 
+    }
+    public void onClick(View button){
+        setContentView(R.layout.activity_log_obj);
     }
 
 }
